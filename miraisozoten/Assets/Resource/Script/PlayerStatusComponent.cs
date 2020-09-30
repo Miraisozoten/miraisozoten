e3dc8ec584911386eeed63a8a5201e264febb9ca
Asuka1905 <asu.aka.051612>
2020-10-01 01:04:25 +0900

    ㏋バー　ー＞　ゲージとメモリ完成 Scene/asuka/SampleScene＜ー㏋バー(ゲージ、メモリ)完成入ってます。 操作方法　F1：ゲージ増加、F2：ゲージ減少、F3：メモリ増加、F4：メモリ減少 ーーーーHP値操作方法ーーーー Hierarchy/unitychan_dynamic_locomotion選択 Inspector/Player Status Component選択 この中の値を操作すると変化する　

---
+++ miraisozoten/Assets/Resource/Script/PlayerStatusComponent.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatusComponent : StatusComponent
+{
+    [SerializeField, Header("HP_Icon")]
+    public float HP_Icon;
+    [SerializeField, Header("HP-Gauge")]
+    public Slider HP_Gauge;
+    [SerializeField, Header("HP-Color")]
+    public Image BerImage;
+    [SerializeField, Header("体力減少速度/s")]
+    public float HP_Decrease;
+    [SerializeField, Header("最大体力(ゲージ)")]
+    public float HP_Max_Gauge;
+    [SerializeField, Header("体力減少量/ハート半分")]
+    public int HP_Amount;
+    [SerializeField, Header("最大体力(アイコン数)")]
+    public int HP_Max_Icon;
+    [SerializeField, Header("体力アイコン左")]
+    public GameObject LeftIcon;
+    [SerializeField, Header("体力アイコン右")]
+    public GameObject RightIcon;
+    [SerializeField, Header("体力アイコンパネル")]
+    public GameObject IconPanel;
+
+    //[SerializeField, Header("カラーG")]
+    private float Color_G;
+    //[SerializeField, Header("カラーR")]
+    private float Color_R;
+    [SerializeField, Header("アイコンリスト")]
+    public List<GameObject> LifeList;
+    [SerializeField, Header("アイコン減少ボタン間隔/s")]
+    public float ButtonInterval;
+    private float Timebutton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //HP = 100;
+        HP = HP_Max_Gauge;
+        HP_Icon = HP_Max_Icon;
+
+        for (int i = 0; i < HP_Max_Icon; i++)
+        {
+            LifeList.Add(Instantiate<GameObject>(LeftIcon, IconPanel.transform));
+            LifeList.Add(Instantiate<GameObject>(RightIcon, IconPanel.transform));
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Timebutton += Time.fixedDeltaTime;
+
+        if (Input.GetKey(KeyCode.F1))
+        {
+            HP += HP_Decrease * Time.fixedDeltaTime;
+        }
+        if (Input.GetKey(KeyCode.F2))
+        {
+            HP -= HP_Decrease * Time.fixedDeltaTime;
+        }
+        if (Input.GetKey(KeyCode.F3) && Timebutton > ButtonInterval)
+        {
+            Timebutton = 0;
+            HP_Icon += HP_Amount * 0.5f;
+        }
+        if (Input.GetKey(KeyCode.F4) && Timebutton > ButtonInterval)
+        {
+            Timebutton = 0;
+            HP_Icon -= HP_Amount * 0.5f;
+        }
+
+        if (HP_Max_Icon >= HP_Icon)
+        {
+            IconAll();
+        }
+        else
+        {
+            HP_Icon = HP_Max_Icon;
+        }
+        if (Input.GetKeyDown(KeyCode.F5) || LifeList.Count != HP_Max_Icon * 2) 
+        {
+            IconReset();
+        }
+
+        GaugeAll();
+    }
+
+    void GaugeAll()
+    {
+        HP_Gauge.value = HP / HP_Max_Gauge;
+        Color_G = 2.0f - (1.0f - HP_Gauge.value) * 2;
+        Color_R = 2.0f - HP_Gauge.value * 2;
+        BerImage.color = new Vector4(Color_R, Color_G, 0, BerImage.color.a);
+        if (HP > HP_Max_Gauge)
+        {
+            HP = HP_Max_Gauge;
+        }
+        if (HP < 0)
+        {
+            HP = 0;
+        }
+    }
+
+    void IconAll()
+    {
+        //HP_Icon変動時
+        if (HP_Icon >= 0)
+        {
+            for (int i = 0; i < LifeList.Count; i++)
+            {
+                if (i < HP_Icon * 2)
+                {
+                    if (LifeList[i].active == false)
+                    {
+                        LifeList[i].SetActive(true);
+                    }
+                }
+                else
+                {
+                    if (LifeList[i].active == true)
+                    {
+                        LifeList[i].SetActive(false);
+                    }
+                }
+            }
+            //if (IconPanel.transform.childCount > HP_Icon)
+            //{
+            //    for (int i = IconPanel.transform.childCount; i > HP_Icon; i--)
+            //    {
+            //        //　ライフゲージoff
+            //        LifeList[i].SetActive(false);
+            //        //Destroy(IconPanel.transform.GetChild(i).gameObject);
+            //        //Destroy(transform.GetChild(transform.childCount - 1 - i).gameObject);
+            //    }
+            //}else if(IconPanel.transform.childCount < HP_Icon)
+            //{
+            //    for (int i = IconPanel.transform.childCount; i > HP_Icon; i--)
+            //    {
+            //        //　ライフゲージoff
+            //        LifeList[i].SetActive(false);
+            //    }
+            //}
+        }
+        if (HP_Icon > HP_Max_Icon)
+        {
+            HP_Icon = HP_Max_Icon;
+        }
+        if (HP_Icon < 0)
+        {
+            HP_Icon = 0;
+        }
+    }
+
+    void IconReset()
+    {
+        //HP_Max_Iconが変動時一度リセット
+        LifeList.Clear();
+
+        //HP設定
+        HP_Icon = HP_Max_Icon;
+        //削除
+        for (int i = 0; i < IconPanel.transform.childCount; i++)
+        {
+            Destroy(IconPanel.transform.GetChild(i).gameObject);
+        }
+        //生成
+        for (int i = 0; i < HP_Max_Icon; i++)
+        {
+            LifeList.Add(Instantiate<GameObject>(LeftIcon, IconPanel.transform));
+            LifeList.Add(Instantiate<GameObject>(RightIcon, IconPanel.transform));
+        }
+    }
+}
+
