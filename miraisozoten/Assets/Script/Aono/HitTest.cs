db96415eb2d2de7facd53e41e9510266044bd9ba
Teron0214 <49426268+Teron0214@users.noreply.github.com>
2020-10-09 12:52:57 +0900

    当たり判定のテスト追加

---
+++ miraisozoten/Assets/Script/Aono/HitTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitTest : MonoBehaviour
+{
+     [SerializeField]
+    NavTest script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Weapon")
+        {
+            Debug.Log("攻撃が当たった");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
