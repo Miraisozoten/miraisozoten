0de16240e24cf47a579128004c0d6019be82c1a2
Teron0214 <49426268+Teron0214@users.noreply.github.com>
2020-10-09 10:07:34 +0900

    追跡範囲、攻撃範囲（仮）の設定を追加

---
+++ miraisozoten/Assets/Script/Aono/ChaseFlag.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseFlag : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    NavTest script;
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            script.chase = true;
+            Debug.Log("追跡中");
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+           script.chase = false;
+            Debug.Log("見失った");
+        }
+    }
+}
--- miraisozoten/Assets/Script/Aono/PlayerMoveTest.cs
+++ miraisozoten/Assets/Script/Aono/PlayerMoveTest.cs
@@ -13,9 +13,25 @@ public class PlayerMoveTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.1f);
         }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x - 0.1f, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
+        }
     }
+
 }
