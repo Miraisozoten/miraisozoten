40fa44600db93a5b52d342893a76c7983710966f
Teron0214 <49426268+Teron0214@users.noreply.github.com>
2020-10-16 13:02:01 +0900

    リスポーンするスクリプトを作成/青りんごのザコ敵を追加（モデルはScript>Aonoの中にあります）

---
+++ miraisozoten/Assets/Script/Aono/DeathFlag.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathFlag : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        Debug.Log("a");
+
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
