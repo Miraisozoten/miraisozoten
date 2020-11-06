b0a0c329d4d19e84b089f313576853320bccc6f7
Teron0214 <49426268+Teron0214@users.noreply.github.com>
2020-11-06 12:09:51 +0900

    リスポーン改修（４種類リスポーンできるよう対応）

--- miraisozoten/Assets/Script/Aono/Respawn.cs
+++ miraisozoten/Assets/Script/Aono/Respawn.cs
@@ -4,24 +4,58 @@ using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
-    [SerializeField, HeaderAttribute("リスポーンさせる敵")]
-
-    
-
+    [SerializeField, HeaderAttribute("リスポーン敵1")]
     public GameObject OriginObject;
 
+   // public string EnemyTag1;
+
+    [SerializeField, HeaderAttribute("リスポーン敵1の数")]
     public int EnemyNum;
 
+
+    [SerializeField, HeaderAttribute("リスポーン敵2")]
+    public GameObject OriginObject2;
+
+    //public string EnemyTag2;
+
+    [SerializeField, HeaderAttribute("リスポーン敵2の数")]
+    public int EnemyNum2;
+
+    [SerializeField, HeaderAttribute("リスポーン敵3")]
+    public GameObject OriginObject3;
+
+    //public string EnemyTag3;
+
+    [SerializeField, HeaderAttribute("リスポーン敵3の数")]
+    public int EnemyNum3;
+
+    [SerializeField, HeaderAttribute("リスポーン敵4")]
+    public GameObject OriginObject4;
+
+    //public string EnemyTag4;
+
+    [SerializeField, HeaderAttribute("リスポーン敵4の数")]
+    public int EnemyNum4;
+
+    string EnemyName;
+
+    string EnemyName2;
+
+    [SerializeField, HeaderAttribute("リスポーンX座標")]
     public float RespawnX;
 
+    [SerializeField, HeaderAttribute("リスポーンY座標")]
     public float RespawnY;
 
+    [SerializeField, HeaderAttribute("リスポーン座標")]
     public float RespawnZ;
 
     private float RandomNum;
 
+    [SerializeField, HeaderAttribute("リスポーン座標の振れ幅の上")]
     public float RandamRange_Tops;
 
+    [SerializeField, HeaderAttribute("リスポーン座標の振れ幅の下")]
     public float RandamRange_Bottom;
 
     private float TImer = 0.0f;
@@ -31,11 +65,23 @@ public class Respawn : MonoBehaviour
     private float ResTime = 10.0f;
 
     GameObject[] tagObjects;
+    GameObject[] tagObjects2;
+    GameObject[] tagObjects3;
+    GameObject[] tagObjects4;
 
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(OriginObject, new Vector3(RespawnX, RespawnY, RespawnZ), Quaternion.identity);
+
+        Instantiate(OriginObject2, new Vector3(RespawnX, RespawnY, RespawnZ), Quaternion.identity);
+
+        EnemyName = OriginObject.name;
+
+        EnemyName2 = OriginObject2.name;
+
+        Debug.Log(EnemyName + "(Clone)");
+        Debug.Log(EnemyName2);
     }
 
     // Update is called once per frame
@@ -49,7 +95,7 @@ public class Respawn : MonoBehaviour
 
         if (TImer > Interval)
         {
-            Check("Enemy");
+            Check(OriginObject.tag);
             TImer = 0;
         }
 
@@ -64,11 +110,29 @@ public class Respawn : MonoBehaviour
 
     void Check(string tagname)
     {
-        tagObjects = GameObject.FindGameObjectsWithTag("Enemy");
+        tagObjects = GameObject.FindGameObjectsWithTag(OriginObject.tag);
+        tagObjects2 = GameObject.FindGameObjectsWithTag(OriginObject2.tag);
+        tagObjects3 = GameObject.FindGameObjectsWithTag(OriginObject3.tag);
+        tagObjects4 = GameObject.FindGameObjectsWithTag(OriginObject4.tag);
         //Debug.Log(tagObjects.Length);
         if (tagObjects.Length <= EnemyNum)
         {
             Instantiate(OriginObject, new Vector3(RespawnX+RandomNum, RespawnY, RespawnZ+RandomNum), Quaternion.identity);
+        }
+
+        if (tagObjects2.Length <= EnemyNum2)
+        {
+            Instantiate(OriginObject2, new Vector3(RespawnX + RandomNum, RespawnY, RespawnZ + RandomNum), Quaternion.identity);
+        }
+
+        if (tagObjects3.Length <= EnemyNum3)
+        {
+            Instantiate(OriginObject3, new Vector3(RespawnX + RandomNum, RespawnY, RespawnZ + RandomNum), Quaternion.identity);
+        }
+
+        if (tagObjects4.Length <= EnemyNum4)
+        {
+            Instantiate(OriginObject4, new Vector3(RespawnX + RandomNum, RespawnY, RespawnZ + RandomNum), Quaternion.identity);
         }
     }
 }
