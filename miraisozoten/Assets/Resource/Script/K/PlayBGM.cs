afe50835b19dea952e66e13a5589871675b8e746
安藤 海斗 <godeater8636@gmail.com>
2020-11-09 12:02:05 +0900

    BGM追加

---
+++ miraisozoten/Assets/Resource/Script/K/PlayBGM.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KanKikuchi.AudioManager;
+
+public class PlayBGM : MonoBehaviour
+{
+    public bool playing = false;
+
+    public string SoundName = "BGM/";
+    // Start is called before the first frame update
+    void Start(){ Play();}
+
+    // Update is called once per frame
+    void Update(){}
+
+    void Play()
+    {
+        switch (playing)
+        {
+            case (true):
+                break;
+            case (false):
+                BGMManager.Instance.Play(SoundName);
+                playing = true;
+                break;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Stop();
+    }
+
+    //一時停止の再開
+    void UnPause()  { BGMManager.Instance.UnPause(SoundName);   }
+    //一時停止
+    void Pause()    { BGMManager.Instance.Pause(SoundName);     }
+    //フェードアウト
+    void FadeOut()  { BGMManager.Instance.FadeOut(SoundName);   }
+    //フェードイン
+    void FadeIn()   { BGMManager.Instance.FadeIn(SoundName);    }
+    //停止
+    void Stop()     { BGMManager.Instance.Stop(SoundName);      }
+
+}
