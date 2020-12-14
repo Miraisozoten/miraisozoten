f4f3e2869a55b1903c5212ba954e1ff2f5920f6a
安藤 海斗 <godeater8636@gmail.com>
2020-12-14 14:17:05 +0900

    Tutorialのシーン,オブジェクト,テクスチャ等追加

---
+++ miraisozoten/Assets/Resource/Script/K/Tutorial/MoveTutorial.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTutorial : Tutorial
+{
+    //インスペクター側で設定
+    public GameObject img;
+    public GameObject img2;
+
+    public KeyCode avoidance;
+
+    // Start is called before the first frame update
+    override protected void Start()
+    {
+        //エラーログ
+        if (img == null)
+            Debug.LogError("imgの値がnullです。");
+        if (img2 == null)
+            Debug.LogError("img2の値がnullです。");
+        //imgの作成
+        img = CreateUIObject(img);
+    }
+
+    // Update is called once per frame
+    override protected void Update()
+    {
+        if (img == null && img2 != null)
+        {
+            GetAvoidanceKey();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            DestroyObject(img);
+
+            img2 = CreateUIObject(img2);
+        }
+    }
+
+    void GetAvoidanceKey()
+    {
+        if (Input.GetKeyDown(avoidance))
+        {
+            DestroyObject(img2);
+            Destroy(this.gameObject);
+        }
+    }
+}
